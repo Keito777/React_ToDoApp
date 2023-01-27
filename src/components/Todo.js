b58977e087abed537d69473b1661a7e3ea2f5d1b
KeitoYuasa <>
2023-01-27 11:40:48 +0900

    update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,11 +6,11 @@ const Todo = ({todo, handleTodoClick}) => {
   };
   
   return (
-    <div>
+    <div className="todo">
         <label>
             <input type="checkbox" onChange={TodoClick}/>
+            <span>{todo.name}</span>
         </label>
-        {todo.name}
     </div>
   );
 };
